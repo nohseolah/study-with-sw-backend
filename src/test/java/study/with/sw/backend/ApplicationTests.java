739666e787caf23fe9180c4e43ac29e4a1b64536
nohseolah <150778072+nohseolah@users.noreply.github.com>
2024-04-03 16:04:37 +0900

    study_0403

---
+++ src/test/java/study/with/sw/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.with.sw.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
